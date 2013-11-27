dbdb7342aa9fecb689dc38c33a9f617498f3fe61
nethergrim <c2q9450@gmail.com>
2013-11-27 12:14:50 +0200

    Update HistoryActivity.java

--- src/com/nethergrim/combogymdiary/HistoryActivity.java
+++ src/com/nethergrim/combogymdiary/HistoryActivity.java
@@ -94,4 +94,14 @@ public class HistoryActivity extends Activity {
 	    super.onDestroy();
 	    db.close();
 	  }
+	  
+	@Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                case android.R.id.home:
+                        NavUtils.navigateUpFromSameTask(this);
+                        return true;
+                }
+                return super.onOptionsItemSelected(item);
+        }
 }
