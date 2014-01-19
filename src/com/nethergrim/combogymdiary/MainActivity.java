a9597b02530c2765903eb27095240b5680c241ff
nethergrim <c2q9450@gmail.com>
2014-01-19 16:26:57 +0200

    1.3.7
    релиз 1.3.7

--- src/com/nethergrim/combogymdiary/MainActivity.java
+++ src/com/nethergrim/combogymdiary/MainActivity.java
@@ -3,6 +3,7 @@ package com.nethergrim.combogymdiary;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -28,41 +29,51 @@ public class MainActivity extends BasicMenuActivity {
 	private InitTask task;
 	public static MainActivity ma;
 	
-
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+	  super.onConfigurationChanged(newConfig);
+	  initUi();
+	}
+	
+	private void initUi(){
+		 mMenuDrawer.setContentView(R.layout.activity_main);
+	        btnSettings   = (Button) findViewById(R.id.buttonSettings);
+	        btnStartT     = (Button) findViewById(R.id.buttonStartTraining);
+	        btnExcersises = (Button) findViewById(R.id.buttonExcersisesList);
+	        btnWorklog    = (Button) findViewById(R.id.btnWorklog);
+	        btnCatalog	  = (Button) findViewById(R.id.btnCataloginMain);
+	        btnMeasurements=(Button) findViewById(R.id.btnMeasurementsS);
+	        pb = (ProgressBar)findViewById(R.id.progressBar1);
+	        pb.setVisibility(View.GONE);
+	        btnMeasurements.setOnClickListener(this);
+	        btnCatalog.setOnClickListener(this);
+	        btnSettings.setOnClickListener(this);
+	        btnExcersises.setOnClickListener(this);
+	        btnStartT.setOnClickListener(this);
+	        btnWorklog.setOnClickListener(this);
+	        db = new DB(this);
+			db.open();
+			cursor = db.getDataExe(null, null, null, null, null, null);
+	        ma=this;
+	        sp = PreferenceManager.getDefaultSharedPreferences(this);
+	        getActionBar().setTitle(R.string.app_name);
+	        if (sp.getBoolean(TRAINING_AT_PROGRESS, false)){
+				Intent intent_to_trainng = new Intent(this,TrainingAtProgress.class);
+		        startActivity(intent_to_trainng);
+			}
+	}
 
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mMenuDrawer.setContentView(R.layout.activity_main);
-        btnSettings   = (Button) findViewById(R.id.buttonSettings);
-        btnStartT     = (Button) findViewById(R.id.buttonStartTraining);
-        btnExcersises = (Button) findViewById(R.id.buttonExcersisesList);
-        btnWorklog    = (Button) findViewById(R.id.btnWorklog);
-        btnCatalog	  = (Button) findViewById(R.id.btnCataloginMain);
-        btnMeasurements=(Button) findViewById(R.id.btnMeasurementsS);
-        pb = (ProgressBar)findViewById(R.id.progressBar1);
-        pb.setVisibility(View.GONE);
-        btnMeasurements.setOnClickListener(this);
-        btnCatalog.setOnClickListener(this);
-        btnSettings.setOnClickListener(this);
-        btnExcersises.setOnClickListener(this);
-        btnStartT.setOnClickListener(this);
-        btnWorklog.setOnClickListener(this);
-        ma=this;
-        sp = PreferenceManager.getDefaultSharedPreferences(this);
-        getActionBar().setTitle(R.string.app_name);
-        db = new DB(this);
-		db.open();
-		cursor = db.getDataExe(null, null, null, null, null, null);
+       initUi();
+      
 		if (cursor.getCount() < 10) {
 			pb.setVisibility(View.VISIBLE);
 			task = new InitTask();
 			task.execute();
 		}
-		if (sp.getBoolean(TRAINING_AT_PROGRESS, false)){
-			Intent intent_to_trainng = new Intent(this,TrainingAtProgress.class);
-	        startActivity(intent_to_trainng);
-		}
+		
 		
     }
 
