2988a3e30e3057a73e34ff380b5b6e8506c78a99
nethergrim <c2q9450@gmail.com>
2014-04-21 12:31:46 +0300

    1.4.7
    Исправил ошибку с версией Google Play Services
    Добавил вьюшку для карточек

--- gen/kankan/wheel/R.java
+++ gen/kankan/wheel/R.java
@@ -8,7 +8,7 @@ package kankan.wheel;
 
 public final class R {
 	public static final class drawable {
-		public static final int wheel_bg = 0x7f0200d5;
-		public static final int wheel_val = 0x7f0200d7;
+		public static final int wheel_bg = 0x7f0200d6;
+		public static final int wheel_val = 0x7f0200d8;
 	}
 }
